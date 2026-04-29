261293c69a525134c1adca34df1efc9977e70699
AstarophTY <astarophty@gmail.com>
2026-04-29 13:22:52 +0200

    fix normes

--- src/ui/menu/cheat_menu.py
+++ src/ui/menu/cheat_menu.py
@@ -1,13 +1,6 @@
-from typing import Callable
-from ursina import Entity, Text, camera, color, mouse, window, Button, Slider
-
-from ..components.button import MenuButton
-
-
-from ursina import *
+from ursina import Entity, Text, camera, color, mouse, Button, Slider, Ursina
 
 font_path = "assets/fonts/PressStart2P-vaV7.ttf"
-
 
 
 class Cheat():
@@ -17,7 +10,7 @@ class Cheat():
         self.is_cursor = is_cursor
         self.is_button = is_button
 
-    def display(self, parent, i):
+    def display(self, parent, i) -> None:
         if self.is_cursor:
             slider_cheat(parent, self.name, i, self.change_state)
         elif self.is_button:
@@ -25,8 +18,9 @@ class Cheat():
         else:
             checkbox_cheat(parent, self.name, i, self.change_state)
 
-    def change_state(self, state):
+    def change_state(self, state) -> None:
         self.state = state
+
 
 class Cheat_menu():
     def __init__(self):
@@ -41,31 +35,30 @@ class Cheat_menu():
         Text(
             text="Cheats",
             parent=self.menu,
-            origin=(0,-7),
+            origin=(0, -7),
             color=color.red,
             scale=2.4,
             z=-0.1,
             font=font_path,
         )
 
-    def show(self):
+    def show(self) -> None:
         self.menu.enabled = True
         mouse.locked = False
 
-    def hide(self):
+    def hide(self) -> None:
         self.menu.enabled = False
         mouse.locked = True
 
-    def add_cheat(self, cheat: Cheat, i):
+    def add_cheat(self, cheat: Cheat, i) -> None:
         self.__cheats.append(cheat)
         cheat.display(self.menu, i)
 
-    def get_cheat(self, name: str):
+    def get_cheat(self, name: str) -> None:
         for cheat in self.__cheats:
             if cheat.name == name:
                 return cheat
         return None
-
 
 
 class slider_cheat(Slider):
@@ -78,7 +71,7 @@ class slider_cheat(Slider):
         super().__init__(
             parent=parent,
             x=0.1,
-            y = self.pos_y,
+            y=self.pos_y,
             min=5,
             max=30,
             z=-0.1,
@@ -103,13 +96,12 @@ class slider_cheat(Slider):
         self.knob.color = color.white
         self.knob.scale = 2
 
-    def update(self):
+    def update(self) -> None:
         super().update()
         if self.value == self.last_value:
             return
         self._change_state(self.value)
         self.last_value = self.value
-
 
 
 class button_cheat(Button):
@@ -148,8 +140,7 @@ class button_cheat(Button):
             origin=(0.05, 0.05),
         )
 
-
-    def on_click(self):
+    def on_click(self) -> None:
         self._change_state(True)
 
 
@@ -172,7 +163,7 @@ class checkbox_cheat(Button):
             parent=parent,
             scale=1,
             color=color.red,
-            origin=(-0.5,0),
+            origin=(-0.5, 0),
             x=-self.pos_x,
             y=self.pos_y,
             z=-0.1,
@@ -189,158 +180,12 @@ class checkbox_cheat(Button):
             z=-0.2
         )
 
-
-    def on_click(self):
+    def on_click(self) -> None:
         self.activated = not self.activated
         self._change_state(self.activated)
         self.indicator.color = color.red if self.activated else color.black
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     app = Ursina()
     app.run()
-
-
-# class CheatsMenu:
-#     def __init__(self):
-#         self._cheats = []
-#         self.panel = None
-#         self.scroll_area = None
-#         self.content = None
-#         self.scroll_offset = 0.0
-#         self.content_height = 0.0
-#         self.visible_height = 0.0
-#         self._button_height = 0.0
-#         self._row_spacing = 0.0
-
-#     def add_button(self, on_click: Callable[[], None], label: str):
-#         self._cheats.append(MenuButton(text=label, on_click=on_click, y=0.2))
-
-#     def display_cheats(self):
-#         if self.panel:
-#             self.panel.disable()
-
-#         aspect = max(window.aspect_ratio, 1.0)
-#         panel_width = min(1.15, max(0.62, 0.42 * aspect))
-#         panel_height = 0.86
-#         button_width = 0.62
-#         button_height = 0.15
-#         row_spacing = 0.20
-#         self._button_height = button_height
-#         self._row_spacing = row_spacing
-
-#         self.panel = Entity(
-#             parent=camera.ui,
-#             model='quad',
-#             color=color.black66,
-#             scale=(panel_width, panel_height),
-#             enabled=True,
-#             collider='box',
-#         )
-
-#         Text(
-#             "Cheat",
-#             parent=self.panel,
-#             y=0.36,
-#             scale=1.8,
-#             origin=(0, 0),
-#         )
-
-#         self.scroll_area = Entity(
-#             parent=self.panel,
-#             y=0.14,
-#             scale=(0.94, 0.72),
-#             collider='box',
-#         )
-#         self.content = Entity(parent=self.scroll_area)
-
-#         for i, button in enumerate(self._cheats):
-#             button.parent = self.content
-#             button.y = -i * row_spacing
-#             button.scale = (button_width, button_height)
-
-#             if hasattr(button, "_base_y"):
-#                 button._base_y = button.y
-#                 button._pressed_y = button.y - 0.008
-#             if hasattr(button, "_base_width"):
-#                 button._base_width = button_width
-#                 button._base_height = button_height
-#                 button._hover_width = button_width * 1.03
-#                 button._hover_height = button_height * 1.03
-
-#         self.scroll_offset = 0
-#         if self._cheats:
-#             self.content_height = (
-#                 (len(self._cheats) - 1) * row_spacing
-#                 + button_height
-#             )
-#         else:
-#             self.content_height = 0
-#         self.visible_height = self.scroll_area.scale_y
-#         self._update_buttons_visibility()
-
-#         self.panel.input = self.handle_scroll
-
-#     def _update_buttons_visibility(self) -> None:
-#         if not self.scroll_area or not self.content:
-#             return
-
-#         half_visible_height = self.scroll_area.scale_y / 2
-#         half_button_height = self._button_height / 2
-
-#         for button in self._cheats:
-#             local_y = self.content.y + button.y
-#             is_visible = (
-#                 -half_visible_height - half_button_height
-#                 <= local_y
-#                 <= half_visible_height + half_button_height
-#             )
-#             button.visible = is_visible
-#             button.enabled = is_visible
-
-#     def handle_scroll(self, key):
-#         hovered = mouse.hovered_entity
-#         is_menu_hovered = False
-#         while hovered is not None:
-#             if hovered in [self.panel, self.scroll_area, self.content]:
-#                 is_menu_hovered = True
-#                 break
-#             hovered = hovered.parent
-
-#         is_button_hovered = any(
-#             mouse.hovered_entity == btn for btn in self._cheats
-#         )
-#         if is_menu_hovered or is_button_hovered:
-#             max_scroll = max(
-#                 0,
-#                 self.content_height - self.visible_height,
-#             )
-
-#             if key == 'scroll up':
-#                 self.scroll_offset = max(0, self.scroll_offset - 0.1)
-#                 self.content.y = self.scroll_offset
-#                 self._update_buttons_visibility()
-#             elif key == 'scroll down':
-#                 self.scroll_offset = min(max_scroll, self.scroll_offset + 0.1)
-#                 self.content.y = self.scroll_offset
-#                 self._update_buttons_visibility()
-
-
-# if __name__ == "__main__":
-#     from ursina import Ursina
-#     app = Ursina()
-#     # cheats = CheatsMenu()
-#     # cheats.add_button(on_click=lambda: print("test"), label="Salut1")
-#     # cheats.add_button(on_click=lambda: print("test"), label="Salut2")
-#     # cheats.add_button(on_click=lambda: print("test"), label="Salut3")
-#     # cheats.add_button(on_click=lambda: print("test"), label="Salut4")
-#     # cheats.add_button(on_click=lambda: print("test"), label="Salut5")
-#     # cheats.add_button(on_click=lambda: print("test"), label="Salut6")
-#     # cheats.add_button(on_click=lambda: print("test"), label="Salut7")
-#     # cheats.add_button(on_click=lambda: print("test"), label="Salut8")
-#     # cheats.add_button(on_click=lambda: print("test"), label="Salut9")
-#     # cheats.add_button(on_click=lambda: print("test"), label="Salut10")
-#     # cheats.add_button(on_click=lambda: print("test"), label="Salut11")
-#     # cheats.add_button(on_click=lambda: print("test"), label="Salut12")
-#     # cheats.display_cheats()
-#     app.run()
