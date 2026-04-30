712e31ddd505ff534a16248c19c150f9378b14ee
AstarophTY <astarophty@gmail.com>
2026-04-30 16:50:27 +0200

    remove settings menu

--- src/ui/menu/__init__.py
+++ src/ui/menu/__init__.py
@@ -1,7 +1,6 @@
 from .main_menu import run_main_menu
 from .end_screen import show_game_over_screen, show_victory_screen
 from .pause_menu import PauseMenuManager
-from .settings_menu import show_settings_menu
 
 
 __all__ = [
