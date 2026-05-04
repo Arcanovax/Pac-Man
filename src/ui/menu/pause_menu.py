e7e3480e9297605e90c7294ff9ff2f9298d40b07
AstarophTY <astarophty@gmail.com>
2026-05-04 11:33:45 +0200

    remove vhs in pause menu

--- src/ui/menu/pause_menu.py
+++ src/ui/menu/pause_menu.py
@@ -9,7 +9,7 @@ from ursina import (
 )
 
 from ...logger import Logger
-from ..components import MenuButton, VHSEffect
+from ..components import MenuButton
 
 
 class PauseMenuManager:
@@ -68,9 +68,6 @@ class PauseMenuManager:
     def _build_pause_ui(self) -> None:
         """Construct the pause menu UI and wire button callbacks."""
         self.pause_entities.extend(self._build_retro_frame())
-
-        vhs = VHSEffect(intensity=0.50)
-        vhs.z = 0.05
 
         title = Entity(
             parent=camera.ui,
